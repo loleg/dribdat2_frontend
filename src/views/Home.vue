b295446cbc374a984d7ce8fdc6097b973f22ac6c
Guordek <mickael.coluccia@gmail.com>
2019-03-12 14:41:37 +0100

    Add ChallengePitch component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <!-- https://www.dailymotion.com/video/xkq3cr -->
+    <!-- https://www.youtube.com/watch?v=zihJTimjdls -->
+    <ChallengePitch pitch="https://www.dailymotion.com/video/xkq3cr"></ChallengePitch>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
+import ChallengePitch from '@/components/ChallengePitch.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    //HelloWorld, 
+    ChallengePitch
   }
 }
 </script>
