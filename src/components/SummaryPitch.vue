f6725abd5337a8633579ccca8dff9e71798808e0
Daniela <daniela.silvalourenco@students.hevs.ch>
2019-03-12 13:10:29 +0100

    First part for the summary description with the test for the summaryPitch

---
+++ src/components/SummaryPitch.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="textSummary">
+        <p>{{ summaryText }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SummaryPitch",
+        props: ['summaryText']
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ProjectPresentation.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="projectPresentation">
+        <SummaryPitch  summary-text="Nihil est enim virtute amabilius"/>
+    </div>
+</template>
+
+<script>
+    import SummaryPitch from '../components/SummaryPitch';
+    export default {
+        name: 'ProjectPresentation',
+        components: {
+            SummaryPitch
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>
