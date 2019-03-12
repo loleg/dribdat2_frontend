e16d0a05ba9e461effee8c6f33b95b4918e5135a
CarolineMoreira <37831348+CarolineMoreira@users.noreply.github.com>
2019-03-12 13:34:25 +0100

    TitleChallenge

--- src/views/ProjectPresentation.vue
+++ src/views/ProjectPresentation.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="projectPresentation">
         <ProjectTitle title="Placeholder Title"></ProjectTitle>
+        <TitleChallenge title="Titre du challenge"/>
         <SummaryPitch  summary-text="Nihil est enim virtute amabilius"/>
     </div>
 </template>
 
 <script>
+    import TitleChallenge from "../components/ProjectPresentation/TitleChallenge";
     import SummaryPitch from '../components/SummaryPitch';
     import ProjectTitle from '../components/ProjectPresentation/ProjectTitle';
     export default {
         name: 'ProjectPresentation',
         components: {
+            TitleChallenge,
             SummaryPitch,
             ProjectTitle
         },
