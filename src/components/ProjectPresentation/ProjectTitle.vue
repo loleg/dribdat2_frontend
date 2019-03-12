a5d5b773336d64217cf8185b89ba84eef780fa15
jonHESSO <jonathan.schnyder@students.hevs.ch>
2019-03-12 13:15:04 +0100

    Added project title component

---
+++ src/components/ProjectPresentation/ProjectTitle.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script>
+    export default {
+        name: "ProjectTitle",
+        props: [
+            'title'
+        ]
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/ProjectPresentation.vue
+++ src/views/ProjectPresentation.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="projectPresentation">
+        <ProjectTitle title="Placeholder Title"></ProjectTitle>
         <SummaryPitch  summary-text="Nihil est enim virtute amabilius"/>
     </div>
 </template>
 
 <script>
     import SummaryPitch from '../components/SummaryPitch';
+    import ProjectTitle from '../components/ProjectPresentation/ProjectTitle';
     export default {
         name: 'ProjectPresentation',
         components: {
-            SummaryPitch
+            SummaryPitch,
+            ProjectTitle
         },
 
     }
