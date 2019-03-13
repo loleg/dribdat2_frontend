512b1e1bd5814c5b323e856b604b552a8ea2fe22
CarolineMoreira <37831348+CarolineMoreira@users.noreply.github.com>
2019-03-13 08:55:30 +0100

    Added TitleChallenge component

---
+++ src/components/ProjectPresentation/TitleChallenge.vue
@@ -0,0 +1,15 @@
+<template>
+        <h1>{{ title }}</h1>
+</template>
+
+<script>
+    export default {
+            name: "TitleChallenge",
+            props: ['title']
+        }
+</script>
+
+
+<style scoped>
+
+</style>
