e38822052717e46301d0cc535f0bf4a04148e5d9
CarolineMoreira <37831348+CarolineMoreira@users.noreply.github.com>
2019-03-13 09:54:13 +0100

    Added Page Info Team with the headlineStatus component

---
+++ src/views/InfoTeam.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="infoTeam">
+        <HeadlineStatus title="Headline Status"></HeadlineStatus>
+    </div>
+</template>
+
+<script>
+
+    import HeadlineStatus from "../components/InfoTeam/HeadlineStatus";
+    export default {
+        name : 'InfoTeam',
+        components: {
+            HeadlineStatus
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>
