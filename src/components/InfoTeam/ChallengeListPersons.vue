4f1751e13d1f829d64ef711a7fdd18264ddc6e17
Guordek <mickael.coluccia@gmail.com>
2019-03-13 12:54:06 +0100

    Add Challenge List of the team and add it in the InfoTeam page

---
+++ src/components/InfoTeam/ChallengeListPersons.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h3>TEAM</h3>
+    <ul>
+      <li v-for="person in list" :key="person">
+        <a :href="person.link">{{ person.firstname + ' ' + person.lastname }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: Array
+  }
+};
+</script>
+
+<style scoped>
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+ 
+li {
+  font: 200 20px/1.5 Helvetica, Verdana, sans-serif;
+  border-bottom: 1px solid #ccc;
+  height: 32px;
+  vertical-align: middle;
+  width: 400px;
+  margin: 0 auto;
+}
+ 
+li:last-child {
+  border: none;
+}
+ 
+li a {
+  text-decoration: none;
+  color: #000;
+  display: block;
+ 
+  -webkit-transition: font-size 0.2s ease, background-color 0.2s ease;
+  -moz-transition: font-size 0.2s ease, background-color 0.2s ease;
+  -o-transition: font-size 0.2s ease, background-color 0.2s ease;
+  -ms-transition: font-size 0.2s ease, background-color 0.2s ease;
+  transition: font-size 0.2s ease, background-color 0.2s ease;
+}
+ 
+li a:hover {
+  font-size: 21px;
+  color: #42b983;
+}
+
+@media screen and (max-width: 992px) {
+  li {
+    width: 100%;
+  }
+}
+
+</style>
--- src/views/InfoTeam.vue
+++ src/views/InfoTeam.vue
@@ -1,19 +1,58 @@
 <template>
-    <div class="infoTeam">
-        <HeadlineStatus status="Headline Status"></HeadlineStatus>
-    </div>
+  <div class="infoTeam">
+    <HeadlineStatus status="Headline Status"></HeadlineStatus>
+    <ChallengeListPersons :list="persons"></ChallengeListPersons>
+  </div>
 </template>
 
 <script>
+import HeadlineStatus from "../components/InfoTeam/HeadlineStatus";
+import ChallengeListPersons from "../components/InfoTeam/ChallengeListPersons";
 
-    import HeadlineStatus from "../components/InfoTeam/HeadlineStatus";
-    export default {
-        name : 'InfoTeam',
-        components: {
-            HeadlineStatus
+export default {
+  name: "InfoTeam",
+  components: {
+    HeadlineStatus,
+    ChallengeListPersons
+  },
+  data() {
+    return {
+      // need to get persons from API
+      persons: [
+        {
+          firstname: "Mickaël",
+          lastname: "Coluccia",
+          link: "#"
         },
-
-    }
+        {
+          firstname: "Jon",
+          lastname: "Schnyderche",
+          link: "#"
+        },
+        {
+          firstname: "Entrique",
+          lastname: "Haziri",
+          link: "#"
+        },
+        {
+          firstname: "Entrique",
+          lastname: "Haziri",
+          link: "#"
+        },
+        {
+          firstname: "Entrique",
+          lastname: "Haziri",
+          link: "#"
+        },
+        {
+          firstname: "Entrique",
+          lastname: "Haziri",
+          link: "#"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped>
