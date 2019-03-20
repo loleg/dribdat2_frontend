551e211953d005c0c5bae6d6b4b86cec322a6071
CarolineMoreira <37831348+CarolineMoreira@users.noreply.github.com>
2019-03-20 10:41:36 +0100

    Button feedback

--- src/views/InfoTeam.vue
+++ src/views/InfoTeam.vue
@@ -2,21 +2,46 @@
     <div class="infoTeam">
         <HeadlineStatus status=project.phase></HeadlineStatus>
         <ChallengeListPersons :list="persons"></ChallengeListPersons>
+
+        <div>
+            <b-button v-b-modal.modal-scrollable>Feedback</b-button>
+
+            <b-modal id="modal-scrollable" scrollable title="Scrollable Content">
+                <p class="my-4" v-for="i in 20" :key="i">
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+                    in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                </p>
+            </b-modal>
+        </div>
+
     </div>
+
+
+
+
+
+
+
 </template>
 
 <script>
     import HeadlineStatus from "../components/InfoTeam/HeadlineStatus";
     import ChallengeListPersons from "../components/InfoTeam/ChallengeListPersons";
 
+
+
     export default {
         name: "InfoTeam",
         components: {
+
             HeadlineStatus,
-            ChallengeListPersons
+            ChallengeListPersons,
         },
+
         data() {
+
             return {
+
                 // need to get persons from API
                 persons: [
                     {
@@ -53,8 +78,10 @@
                 props: [
                     'project'
                 ]
+
             };
-        }
+        },
+
     };
 </script>
 
