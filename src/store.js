0f21cea7ba5e49211bc1512bbea9df53b3838eee
Anthony <anthony.ritz@gmx.ch>
2019-03-22 22:11:18 +0100

    Added github contributors and issues to vuex

--- src/store.js
+++ src/store.js
@@ -1,16 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.use(Vuex)
+Vue.use(VueAxios, axios)
+
+let github_apiURL = 'https://api.github.com'
+let path = '/repos/ChallengeHunt/challengehunt'
 
 export default new Vuex.Store({
   state: {
-
+      github_BaseURL: '',
+      github_repoPath: '/repos/ChallengeHunt/challengehunt',
+      contributors: [],
+      issues: []
   },
   mutations: {
-
+      SET_CONTRIBUTORS (state, contributors) {
+          state.contributors = contributors
+      },
+      SET_ISSUES (state, issues) {
+          state.issues = issues
+      }
   },
   actions: {
-
+      loadContributors ({ commit }) {
+          axios
+              .get(github_apiURL + path + '/stats/contributors')
+              .then(r => r.data)
+              .then(contributors => {
+                  commit('SET_CONTRIBUTORS', contributors)
+              })
+      },
+      loadIssues ({ commit }) {
+          axios
+              .get(github_apiURL + path + '/issues')
+              .then(r => r.data)
+              .then(issues => {
+                  commit('SET_ISSUES', issues)
+              })
+      }
   }
 })
--- src/views/DevelopmentStatus.vue
+++ src/views/DevelopmentStatus.vue
@@ -5,12 +5,21 @@
 </template>
 
 <script>
+    import { mapState } from 'vuex'
     export default {
         name: "DevelopmentStatus",
         components: {
 
         },
-        props: ["project"]
+        props: ["project"],
+        mounted () {
+            this.$store.dispatch('loadContributors')
+            this.$store.dispatch('loadIssues')
+        },
+        computed: mapState([
+            'contributors',
+            'issues'
+        ])
     };
 </script>
 
