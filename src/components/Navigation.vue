e19e9652b99ab60a972eeac7943daa8d7ae4d7a9
Daniela <daniela.silvalourenco@students.hevs.ch>
2019-04-03 10:27:28 +0200

    side bar

---
+++ src/components/Navigation.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="sidebar" >
+
+        <a>
+            <router-link :to="{ name: 'presentation', params: { id:id }}">Project presentation</router-link>
+        </a>
+        <a > <router-link :to="{ name: 'team', params: { id:id }}">Info Team</router-link></a>
+        <a >  <router-link :to="{ name: 'development', params: { id:id }}">Development Status</router-link></a>
+        <a ><router-link :to="{ name: 'ressources', params: { id:id }}">Ressources</router-link></a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navigation",
+        props: ["id"],
+
+
+
+    }
+</script>
+
+<style scoped>
+
+    .sidebar {
+
+        width: 180px;
+        background-color: #616163;
+        position: fixed;
+        height: 100%;
+        border: 1px groove  #89a7af;
+        border-radius: 5px;
+
+    }
+
+    .sidebar a  {
+        display: block;
+        color: #f9f9f9;
+        padding : 15px;
+        text-decoration: none;
+        border-radius: 2px;
+
+
+
+
+    }
+
+
+
+    .sidebar a:hover{
+        background-color: #89a7af;
+        color: #f9f9f9;
+
+
+    }
+
+
+
+    @media screen and (max-width: 900px) {
+        .sidebar {
+
+            width: 100%;
+            position: relative;
+        }
+        .sidebar a {float: top;}
+    }
+
+
+</style>
--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,102 +1,55 @@
 <template>
-  <div id="project">
-    <edit-button :editMode="editMode"></edit-button>
+    <div id="project">
+        <edit-button :editMode="editMode"></edit-button>
+        <navigation :id="this.id"></navigation>
 
-    <div class="sidebar" >
-      <a class="active" >
-        <router-link :to="{ name: 'presentation', params: { id: custom_project.id }}">Project presentation</router-link>
-      </a>
-      <a > <router-link :to="{ name: 'team', params: { id: custom_project.id }}">Info Team</router-link></a>
-      <a >  <router-link :to="{ name: 'development', params: { id: custom_project.id }}">Development Status</router-link></a>
-      <a ><router-link :to="{ name: 'ressources', params: { id: custom_project.id }}">Ressources</router-link></a>
+
+        <div class="content" v-bind:style="{ backgroundColor: blue}">
+            <router-view :project="custom_project"></router-view>
+        </div>
+
     </div>
-
-<div class="content">
-  <router-view :project="custom_project"></router-view>
-</div>
-
-  </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
-import EditButton from "./EditButton.vue";
+    import {mapState} from "vuex";
+    import EditButton from "./EditButton.vue";
+    import Navigation from "./Navigation.vue"
 
-export default {
-  name: "Project",
-  props: ["id"],
-  components: { EditButton },
-  mounted() {
-    this.$store.dispatch("loadCustomProject", this.id);
-  },
-  computed: mapState(["custom_project", "editMode"]),
+    export default {
+        name: "Project",
+        props: ["id"],
+        data: function () {
+            return {
+                backgroundColor: " #89a7af"
+            }
+        },
+        components: {EditButton, Navigation},
+        mounted() {
+            this.$store.dispatch("loadCustomProject", this.id);
+        },
+        computed: mapState(["custom_project", "editMode"]),
 
 
-
-
-
-};
+    };
 </script>
 
 
 <style scoped>
 
 
-  .sidebar {
-
-    width: 180px;
-    background-color: #616163;
-    position: fixed;
-    height: 100%;
-    border: 1px groove  #89a7af;
-    border-radius: 5px;
-  }
-
-  .sidebar a  {
-    display: block;
-    color: #f9f9f9;
-   padding : 15px;
-    text-decoration: none;
-    border-radius: 2px;
-    
-
-
-
-  }
-
-  .sidebar a.active {
-    background-color: #89a7af;
-    color: white;
-    padding : 15px;
-  }
-
-
-.sidebar a:hover{
-  background-color: #89a7af;
-  color: white;
-
-}
-
-  .sidebar a:hover:not(.active) {
-    background-color: #555;
-    color: white;
-
-  }
-
-  .content {
-    margin-left: 200px;
-    padding: 1px 20px;
-  }
-
-  @media screen and (max-width: 700px) {
-    .sidebar {
-      width: 100%;
-      height: auto;
-      position: relative;
+    .content {
+        margin-left: 200px;
+        padding: 1px 20px;
     }
-    .sidebar a {float: left;}
-    div.content {margin-left: 0;}
-  }
+
+    @media screen and (max-width: 700px) {
+        .content {
+            margin-left: 0px;
+            padding: 1px 20px;
+        }
+
+    }
 
 
 
