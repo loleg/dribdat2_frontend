d33e872ddc51719d9626319071e03071359530dd
CarolineMoreira <37831348+CarolineMoreira@users.noreply.github.com>
2019-04-17 16:09:55 +0200

    Corrections design

--- src/views/DevelopmentStatus.vue
+++ src/views/DevelopmentStatus.vue
@@ -115,9 +115,6 @@
     opacity: 0.87;
     width: 100%;
     margin: 0;
-    -webkit-box-shadow: 0px 13px 168px -30px rgba(0,0,0,1);
-    -moz-box-shadow: 0px 13px 168px -30px rgba(0,0,0,1);
-    box-shadow: 0px 13px 168px -30px rgba(0,0,0,1);
     border: 1px groove #333333;
     border-radius: 10px;
   }
